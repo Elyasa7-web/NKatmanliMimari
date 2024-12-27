154f6f2d88ae98dcbc43a0186a28e3de93e109b0
fb_20 <fb_20@ELYASA>
2024-12-28 02:17:02 +0300

    Add project files.

---
+++ NKatmanliMimari/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAccesLayer;
+using EntityLayer;
+using LogicLayer;
+
+namespace NKatmanliMimari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            List<EntityPersonel> Perlist = LogicPersonel.LLPersonelListesi();
+            dataGridView1.DataSource = Perlist;
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Ad = TxtAd.Text;
+            ent.Soyad = TxtSoyad.Text;
+            ent.Maas = short.Parse(TxtMaas.Text);
+            ent.Sehir = TxtSehir.Text;
+            ent.Gorev = TxtGorev.Text;
+            LogicPersonel.LLPersonelEkle(ent);
+            MessageBox.Show("Personel Eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(textBox1.Text);
+            LogicPersonel.LLPersonelSil(ent.Id);
+            MessageBox.Show("Personel Silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(textBox1.Text);
+            ent.Ad = TxtAd.Text;
+            ent.Soyad = TxtSoyad.Text;
+            ent.Maas = short.Parse (TxtMaas.Text);
+            ent.Sehir = TxtSehir.Text;
+            ent.Gorev = TxtGorev.Text;
+            LogicPersonel.LLPersonelGuncelle(ent);
+            MessageBox.Show("Personel Güncellendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
